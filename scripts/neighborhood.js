8ce776111f9870caef2c86be890084a51643a401
Sam Hamm <sam@codefellows.com>
2018-06-08 10:31:01 -0700

    end of class

--- scripts/neighborhood.js
+++ scripts/neighborhood.js
@@ -1,7 +1,7 @@
 var neighborhoods = [];
 
 // REVIEW: This is another way to use a constructor to duplicate an array of raw data objects
-function Neighborhood (rawDataObject) {
+function Neighborhood(rawDataObject) {
   for (var key in rawDataObject) {
     this[key] = rawDataObject[key];
   }
@@ -11,14 +11,17 @@ Neighborhood.prototype.toHtml = function() {
   // REVIEW: This method on each instance of Neighborhood allows that object to create its own HTML
   // TODO: Complete this using Handlebars!!!
   // 1. Get the template from the HTML document
+  let source = $('#neighborhood-template').html();
   // 2. Use Handlebars to "compile" the HTML
+  let templateRender = Handlebars.compile(source);
   // 3. Do not forget to return the HTML from this method
+  return templateRender(this);
 };
 
 neighborhoodDataSet.forEach(function(neighborhoodObject) {
   neighborhoods.push(new Neighborhood(neighborhoodObject));
 });
 
-neighborhoods.forEach(function(ourNewNeighborhoodObject){
+neighborhoods.forEach(function(ourNewNeighborhoodObject) {
   $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
 });
